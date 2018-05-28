c8fb47a35cec73f219fcf8d3f2de96c8af93b38c
Björn Wahle <bjoern.wahle@mail.de>
2018-05-28 11:53:18 +0200

    rule based agent

---
+++ main_rulebased.py
@@ -0,0 +1,98 @@
+import logging
+import sys
+
+import pygame
+
+from ple import PLE
+from sklearn.preprocessing import MinMaxScaler
+
+from Joust import Joust
+from rule_base_agent import RuleBasedAgent
+from test_agent import NaiveAgent
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import sklearn
+
+def process_state(state):
+    return np.array([
+        state['player1_x'],
+        state['player1_x_speed'],
+        state['player1_y'],
+        state['player1_y_speed'],
+        state['player2_x'],
+        state['player2_x_speed'],
+        state['player2_y'],
+        state['player2_y_speed'],
+    ]).reshape(1, -1)[0]
+
+
+game = Joust(display_screen=True)
+
+p = PLE(game, fps=30, display_screen=False, state_preprocessor=process_state, force_fps=False)
+p.init()
+player1 = game.player1
+player2 = game.player2
+agent1 = RuleBasedAgent(player1, game.p1_actions, log_level=logging.INFO)
+agent2 = RuleBasedAgent(player2, game.p2_actions, cooperating=False, log_level=logging.INFO)
+
+game.adjustRewards(
+    {
+        "positive": 0.1,
+        "tick": 0.001,
+        "negative": -0.1,
+        "win": 1,
+        "loss": -1
+    }
+)
+
+nb_frames = 500
+num_epoch = 100
+rewards = np.array([0.0, 0.0])
+observation = 0
+train_start = 1000
+
+r1_list = []
+r2_list = []
+
+for epoch in range(num_epoch):
+    for f in range(nb_frames):
+        if p.game_over():  # check if the game is over
+            p.reset_game()
+            break
+
+        state = p.getGameState()
+
+        # obs = p.getScreenRGB()
+        action1 = agent1.pick_action(state, rewards, observation)
+        action2 = agent2.pick_action(state, rewards, observation)
+        rewards = np.array(p.act([action1, action2]))
+        agent1.store_reward(rewards)
+        agent2.store_reward(rewards)
+
+        observation += 1
+
+    # player 1 rewards
+    final_reward1 = np.array([agent1.rewards]).sum()
+    print("Player 1: %f" % final_reward1)
+    r1_list.append(final_reward1)
+    agent1.rewards = []
+
+    # player 2 rewards
+    final_reward2 = np.array([agent2.rewards]).sum()
+    print("Player 2: %f" % final_reward2)
+    r2_list.append(final_reward2)
+    agent2.rewards = []
+
+    p.reset_game()
+    print("End epoch %i" % epoch)
+
+plt.plot(range(num_epoch), r1_list, color='red')
+plt.plot(range(num_epoch), r2_list, color='blue')
+plt.grid()
+plt.xlabel("Epoch")
+plt.ylabel("Reward")
+plt.show()
+
+pygame.quit()
+sys.exit()
---
+++ rule_base_agent.py
@@ -0,0 +1,83 @@
+import math
+
+import numpy as np
+import logging
+
+
+class RuleBasedAgent():
+    def __init__(self, player, actions, cooperating=True, log_level=logging.WARNING):
+        self.player = player
+        self.id = player.id
+        self.action_keys = actions.keys()
+        self.actions = actions.values()
+        self.rewards = []
+        self.last_state = None
+        self.last_action = None
+
+        self.cooperating = cooperating
+
+        self.logger = self.init_logger(log_level)
+
+    def init_logger(self, log_level):
+        logger = logging.Logger("P%i" % self.id)
+        logger.setLevel(log_level)
+        # create console handler and set level to debug
+        ch = logging.StreamHandler()
+        ch.setLevel(logging.DEBUG)
+        # create formatter
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        # add formatter to ch
+        ch.setFormatter(formatter)
+        # add ch to logger
+        logger.addHandler(ch)
+        return logger
+
+    def pick_action(self, state, rewards, time):
+
+        action_idx = None
+        other_player = self.get_other_player_state(state)
+
+        if self.cooperating:
+            if self.distance_to(other_player['x'], other_player['y']) < 150.0:
+                self.logger.info("I am close")
+                reverted_x_speed, reverted_y_speed = self.get_reverted_vector(other_player['x'], other_player['y'])
+                if reverted_x_speed > reverted_y_speed:
+                    if reverted_x_speed>0:
+                        action_idx = self.get_action_index("right")
+                    else:
+                        action_idx = self.get_action_index("left")
+                else:
+                    if reverted_y_speed>0:
+                        action_idx = self.get_action_index("noop")
+                    else:
+                        action_idx = self.get_action_index("up")
+
+        if self.player.y > 500:
+            action_idx = self.get_action_index("up")
+
+        if action_idx is None:
+            action_idx = np.random.randint(0, len(self.actions))
+
+        self.last_state = state
+        self.last_action = action_idx
+        return self.actions[action_idx]
+
+    def get_action_index(self, action_key):
+        return self.action_keys.index("p%i_%s" % (self.id, action_key))
+
+    def store_reward(self, rewards):
+        self.rewards.append(rewards[self.id - 1])
+
+    def get_other_player_state(self, state):
+        if self.id == 1:
+            p_state = state[4:]
+        else:
+            p_state = state[:4]
+
+        return {'x': p_state[0], 'y': p_state[2], 'x_speed': p_state[1], 'y_speed': p_state[3]}
+
+    def distance_to(self, o_x, o_y):
+        return math.sqrt(((self.player.x - o_x) ** 2) + ((self.player.y - o_y) ** 2))
+
+    def get_reverted_vector(self, x, y):
+        return (x-self.player.x) * -1.0, (y-self.player.y) * -1.0
